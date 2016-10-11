6f7e7cf79367f6cdc1236b863743f6c016dbee0d
cobWeb codename <cobWeb codename>
2016-10-11 16:55:52 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+	$('.b-tabs-content .b-tabs-content__item').not(':first').hide();
+
+	$('.b-tabs-navigation__item').each(function(i){
+		$(this).attr('data-tab','tab' + i);
+	});
+
+	$('.b-tabs-content__item').each(function(i){
+		$(this).attr('data-tab','tab' + i);
+	});
+
+	$('.b-tabs-navigation__item').click(function(e){
+		var dataTab = $(this).data('tab');
+		var getWrapper = $(this).closest('.b-wrapper-tabs');
+
+		getWrapper.find('.b-tabs-navigation__item').removeClass('active');
+		$(this).addClass('active');
+
+		getWrapper.find('.b-tabs-content__item').hide();
+		getWrapper.find('.b-tabs-content__item[data-tab=' + dataTab + ']').show();
+	});
+
+});
